8bca599c814f7ef9642eac0aece777809045317e
PulkitDhirana1211 <pulkitdhirana9@gmail.com>
2023-12-16 02:46:14 +0530

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Pulkit Dhirana on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
